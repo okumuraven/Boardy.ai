8804ef6bc79bcf2f76b9a8fa6d6efdd6f989bffd
Crimzor <your-email@example.com>
2026-06-26 16:11:29 +0300

    commit

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001";
+const DEPLOYER_PRIVATE_KEY =
+  process.env.DEPLOYER_PRIVATE_KEY || process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
