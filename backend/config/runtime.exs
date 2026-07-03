7a350b3c5ae35ad2a94c067302efd266fc00bb80
okumuraven <okumuraven@gmail.com>
2026-07-03 22:27:20 +0300

    fix: re-enable SSL for external database with bypass verification

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -34,7 +34,8 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :boardy, Boardy.Repo,
-    # ssl: true, # Disabled for Render internal DB connections
+    ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
